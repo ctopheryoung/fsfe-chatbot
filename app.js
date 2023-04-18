c1f4cb4465012ee80df15e4f461feb0d5929d66b
Chris Young <ctopheryoung@gmail.com>
2023-04-18 16:00:56 -0700

    create basic websocket server, app server

---
+++ app.js
@@ -0,0 +1,33 @@
+import path, { dirname } from "path";
+import express from "express";
+import { fileURLToPath } from "url";
+import { createServer } from "http";
+import { WebSocketServer } from "ws";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+// Serve HTML
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "ui/index.html"));
+});
+
+// Serve JS
+app.use("/js", express.static("ui/js"));
+
+const server = app.listen(port, () => {
+  console.log(`Chatbot app listening on port ${port}`);
+});
+
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (ws) => {
+  ws.on("error", console.error);
+
+  ws.on("message", (data) => console.log(`Received: ${data}`));
+
+  ws.send("something");
+});
